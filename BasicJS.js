ed9eb4700d45dcb6855e6a69a70bf31901ed547d
Floyd-88 <ilasaz1988@gmail.com>
2022-01-26 21:30:38 +0300

    lesson 1

---
+++ BasicJS.js
@@ -0,0 +1,14 @@
+/*var should not exist in your code.
+
+You should change fCC to all uppercase.
+
+FCC should be a constant variable declared with const.
+
+fact should be declared with let.
+
+console.log should be changed to print the FCC and fact variables.*/
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
