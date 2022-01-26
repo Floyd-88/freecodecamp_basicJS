2898ae17fb297efe87a539a9bdcb5b1236f38eb9
Floyd-88 <ilasaz1988@gmail.com>
2022-01-26 23:11:28 +0300

    lesson 2

--- BasicJS.js
+++ BasicJS.js
@@ -1,14 +1,93 @@
 /*var should not exist in your code.
-
 You should change fCC to all uppercase.
-
 FCC should be a constant variable declared with const.
-
 fact should be declared with let.
-
 console.log should be changed to print the FCC and fact variables.*/
 
-const FCC = "freeCodeCamp"; // Change this line
+/*const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
-console.log(FCC, fact); // Change this line
+console.log(FCC, fact); // Change this line*/
+
+
+
+/*myStr should have a value of the string This is the start. This is the end.
+You should use the + operator to build myStr.
+myStr should be created using the const keyword.
+You should assign the result to the myStr variable.*/
+
+/*const myStr = "This is the start." + " This is the end."; 
+console.log(myStr);*/
+
+
+
+/*myStr should have a value of the string This is the first sentence. This is the second sentence.
+You should use the += operator to build myStr.*/
+
+/*let myStr;
+myStr = "This is the first sentence. ";
+myStr +="This is the second sentence.";*/
+
+
+
+
+/*myName should be set to a string at least 3 characters long.
+You should use two + operators to build myStr with myName inside it.*/
+// Only change code below this line
+
+/*const myName = "Ilia";
+const myStr = "I " + myName + "hello";*/
+
+
+
+
+/*someAdjective should be set to a string at least 3 characters long.
+You should append someAdjective to myStr using the += operator.*/
+// Change code below this line
+
+/*const someAdjective = "JavaScript";
+let myStr = "Learning to code is ";
+myStr += someAdjective;*/
+
+
+
+
+/*You should not change the variable declarations in the // Setup section.
+lastNameLength should be equal to eight.
+You should be getting the length of lastName by using .length like this: lastName.length.*/
+// Setup
+
+/*let lastNameLength = 0;
+const lastName = "Lovelace";*/
+
+
+
+// Only change code below this line
+
+/*lastNameLength = lastName.length;*/
+
+
+
+
+/*The firstLetterOfLastName variable should have the value of L.
+You should use bracket notation.*/
+
+// Setup
+
+/*let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line*/
+
+
+
+/*myStr should have a value of the string Hello World.
+You should not change the code above the specified comment.*/
+// Setup
+
+/*// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line*/
