4f78cc177fa5a9d36e8af70f9a35c4a38d04978d
Floyd-88 <ilasaz1988@gmail.com>
2022-02-02 21:49:30 +0300

    lesson 4

--- BasicJS.js
+++ BasicJS.js
@@ -130,7 +130,125 @@ let removedFromMyArray = myArray.pop()
 
 /*Use the .shift() function to remove the first item from myArray and assign the "shifted off" value to a new variable, removedFromMyArray.*/
 // Setup
-const myArray = [["John", 23], ["dog", 3]];
-const removedFromMyArray = myArray.shift();
+// const myArray = [["John", 23], ["dog", 3]];
+// const removedFromMyArray = myArray.shift();
 
 // Only change code below this line
+
+
+
+/*Add ["Paul", 35] to the beginning of the myArray variable using unshift().*/
+// Setup
+/*const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35])*/
+// Only change code below this line
+
+
+
+/*Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+["Chocolate Bar", 15]*/
+
+/*const myList = [["milk", 2], ["butter", 3], ["apple", 5 ], ["orange", 1], ["bread", 1]];*/
+
+
+/*Create a function called reusableFunction which prints the string Hi World to the dev console.
+Call the function.*/
+
+/*let reusableFunction = () => {
+  console.log("Hi World")
+}
+reusableFunction();*/
+
+
+
+/*Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+Call the function with two numbers as arguments.*/
+
+/*let functionWithArgs = (a, b) => {
+  console.log(a + b);
+} 
+functionWithArgs(1, 2);*/
+
+
+
+/*Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.*/
+
+/*function timesFive(n) {
+  return 5 * n;
+}*/ 
+
+
+/*Using let or const, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10.
+Inside function fun1, assign 5 to oopsGlobal without using the let or const keywords.*/
+
+// Declare the myGlobal variable below this line
+/*let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}*/
+
+
+/*Add a local variable to myOutfit function to override the value of outerWear with the string sweater.*/
+
+// Setup
+/*const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+let outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();*/
+
+
+/*Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.*/
+
+/*// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+} 
+
+// Only change code above this line
+
+addThree();
+addFive();*/
+
+
+
+/*Call the processArg function with an argument of 7 and assign its return value to the variable processed.*/
+
+// Setup
+/*let processed = processArg(7);
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+*/
+// Only change code below this line
+
+
